ed4458cdd489a71d64e2e1dc64b513899cb9405f
ffferncake <fernnichanun@gmail.com>
2024-12-12 09:44:51 +0900

    점수 업데이트

--- src/App.js
+++ src/App.js
@@ -247,8 +247,8 @@ function App() {
         )}
       </div>
       <div className="score-wrap">
-        <p className="score-heading">현재 점수 : 2250 점</p>
-        <p className="score-date">updated : 2024.12.11 (+750점 평일날 와서)</p>
+        <p className="score-heading">현재 점수 : 2750 점</p>
+        <p className="score-date">updated : 2024.12.12 (+500점 평일날 와서)</p>
       </div>
       <div className="slider">
         {Array.from({ length: 10 }, (_, i) => (
